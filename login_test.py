44599261511e01df07f7a2ec0ff3d887f60b96d7
vereniliana <13515078@std.stei.itb.ac.id>
2019-08-29 11:44:19 +0700

    add test_no_phone

--- login_test.py
+++ login_test.py
@@ -2,14 +2,17 @@ from selenium import webdriver
 
 driver = webdriver.Chrome("C:/Users/veren_p0omp/Downloads/chromedriver.exe")
 
-# from webdriver_manager.chrome import ChromeDriverManager
-
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-
-# def site_login():
-driver.get("https://pr.kargo.tech/shipper/welcome/login")
-driver.find_element_by_id("render_phone_number-field").send_keys("82137014747")
-driver.find_element_by_id("render_textfield").send_keys("Kargo123")
-driver.find_element_by_id("button_component-button").click()
+def site_login():
+    driver.get("https://pr.kargo.tech/shipper/welcome/login")
+    driver.find_element_by_id("render_phone_number-field").send_keys("82137014747")
+    driver.find_element_by_id("render_textfield").send_keys("Kargo123")
+    driver.find_element_by_id("button_component-button").click()
 
 # site_login()
+
+def test_no_phone():
+    driver.get("https://pr.kargo.tech/shipper/welcome/login")
+    driver.find_element_by_id("button_component-button").click()
+    a = driver.find_elements_by_xpath("//*[contains(text(), 'Masukkan nomor telepon')]")
+
+test_no_phone()
