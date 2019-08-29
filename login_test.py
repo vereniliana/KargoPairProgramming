16e3917f539ea385430ae9e5733959a835d4c79f
vereniliana <13515078@std.stei.itb.ac.id>
2019-08-29 12:06:06 +0700

    check if url valid

--- login_test.py
+++ login_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time 
 
 driver = webdriver.Chrome("C:/Users/veren_p0omp/Downloads/chromedriver.exe")
 
@@ -7,12 +8,16 @@ def site_login():
     driver.find_element_by_id("render_phone_number-field").send_keys("82137014747")
     driver.find_element_by_id("render_textfield").send_keys("Kargo123")
     driver.find_element_by_id("button_component-button").click()
+    time.sleep(10)
+    url = driver.current_url
+    if (url == "https://pr.kargo.tech/shipper/welcome/home"):
+        print("done")
 
-# site_login()
+site_login()
 
 def test_no_phone():
     driver.get("https://pr.kargo.tech/shipper/welcome/login")
     driver.find_element_by_id("button_component-button").click()
     a = driver.find_elements_by_xpath("//*[contains(text(), 'Masukkan nomor telepon')]")
 
-test_no_phone()
+# test_no_phone()
